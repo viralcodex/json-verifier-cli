05e3f2d75d1d1e990abdaf20c479a2ec4124e558
Aviral Shukla <aviral.shukla2122000@gmail.com>
2024-11-25 18:42:55 +0530

    Minor file changes

--- bin/json-parser.cli.js
+++ bin/json-parser.cli.js
@@ -6,7 +6,7 @@ import verifyFiles from './utils.js';
 
 program.version("0.0.1")
     .description(
-    chalk.cyanBright.bold.underline('JSON PARSER CLI v0.0.1\nCheck whether the JSON files are valid or not and parse one or all of them together.\nJust open the terminal in the directory your files/folders are present and use one of the following commands.'))
+    chalk.cyanBright.bold.underline('JSON VERIFIER CLI v0.0.1\nCheck whether the JSON files are valid or not and parse one or all of them together.\nJust open the terminal in the directory your files/folders are present and use one of the following commands.'))
     .option("-f, --file <file_path>", "provide the file path for the JSON file to be checked")
     .option("-flr, --folder <folder_path>", "provide the folder patah with JSON files to be checked")
     .action((option) => {
